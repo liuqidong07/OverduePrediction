92ff00827234be82b64f27e43222e321693b2497
Qidong <dong_liuqi@163.com>
2021-01-15 15:59:17 +0800

    add data analysis

--- main.py
+++ main.py
@@ -8,3 +8,8 @@
 '''
 
 # here put the import lib
+import torch
+print(torch.cuda.is_available())
+print(torch.cuda.device_count())
+print(torch.cuda.current_device())
+print(torch.cuda.get_device_name(0))
